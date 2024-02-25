589e3d38180616253a2bb1ea9af0995ebd4755f9
yosefha4 <yosefha4@ac.sce.ac.il>
2024-02-25 15:04:55 +0200

    Update auth action (create signup & login)

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,4 +1,7 @@
 const pool = require("../db");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 
 
 exports.getAllUsers = async (req,res) => {
@@ -22,4 +25,41 @@ exports.getUserById = async (req,res) => {
     return res.status(404).send("User not found");
 
   }
+  };
+
+  exports.signup = async (req, res) => {
+    try {
+      const { user_name, user_email, user_address , password } = req.body;
+      const hashedPassword = await bcrypt.hash(password, 10);
+      // res.status(200).send({user_name, user_email, user_address , password });
+      const query = 'INSERT INTO users (user_name, user_email, user_address,password) VALUES ($1, $2, $3, $4) RETURNING *';
+      const { rows } = await pool.query(query, [user_name, user_email, user_address,hashedPassword]);
+      res.status(201).json({ message: 'User created successfully', user: rows[0] });
+    } catch (error) {
+      res.status(500).json({ error: 'Internal server error', message:error });
+    }
+  };
+
+  exports.login = async (req, res) => {
+    try {
+      const { user_name, password } = req.body;
+      const query = 'SELECT * FROM users WHERE user_name = $1';
+      const { rows } = await pool.query(query, [user_name]);
+  
+      if (rows.length === 0) {
+        return res.status(404).json({ error: 'User not found' });
+      }
+  
+      const user = rows[0];
+      const isValidPassword = await bcrypt.compare(password, user.password);
+  
+      if (!isValidPassword) {
+        return res.status(401).json({ error: 'Invalid password' ,row0:rows[0]});
+      }
+  
+      const token = jwt.sign({ id: user.id, username: user.username }, 'your_secret_key', { expiresIn: '1h' });
+      res.status(200).json({ token });
+    } catch (error) {
+      res.status(500).json({ error: 'Internal server error' });
+    }
   };
--- server/routes/UserRoute.js
+++ server/routes/UserRoute.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const UserControl = require('../controllers/UserController')
+const { authenticateToken } = require('../middleware/authMiddleware');
+
 
 router.get('/', UserControl.getAllUsers);
 router.get('/userDetails/:userId', UserControl.getUserById);
+
+// auth routes
+router.post("/signup",UserControl.signup)
+router.post("/login",UserControl.login)
+
+router.get('/profile', authenticateToken, (req, res) => {
+    console.log('User:', req.user); // Check if user data is correctly attached to the request
+
+    res.json(req.user);
+  });
 
 module.exports = router;
