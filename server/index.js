7ae86eda05c0565688eb139f5833a68a1de67922
yosefha4 <yosefha4@ac.sce.ac.il>
2024-03-07 14:39:06 +0200

    Update deps

--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 const pool = require("./db");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
 //Middleware
 app.use(cors());
 app.use(express.json());
+
+app.use(cookieParser());
 
 //Routes
 
