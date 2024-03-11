3e8ac2bb46a0cf84aaad10beacf1922f194ae512
yosefha4 <yosefha4@ac.sce.ac.il>
2024-03-11 12:21:32 +0200

    Install deps & create send auto email func

--- server/controllers/NewsLetterController.js
+++ server/controllers/NewsLetterController.js
@@ -1,29 +1,57 @@
 const pool = require("../db");
+const nodemailer = require("nodemailer");
 
+// Nodemailer transporter configuration
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "yossonline2u@gmail.com",
+    pass: process.env.EMAIL_NEWS_PASS,
+  },
+});
 
-exports.subscribe = async (req,res) => {
-    try {
-        const {user_email} = req.body;
-        if(!user_email || user_email === ""){
-            return res.status(401).send("Email address canot be an empty string");
-        }
+// Function to send confirmation email
+async function sendConfirmationEmail(email) {
+  try {
+    // Email content
+    const mailOptions = {
+      from: "yossonline2u@gmail.com",
+      to: email,
+      subject: "Newsletter Subscription Confirmation",
+      text: "Thank you for subscribing to our newsletter!",
+    };
 
-        const emailExist = await pool.query(
-            "SELECT * FROM subscriptions WHERE email = $1",
-            [user_email]
-          );
-        if(emailExist.rows.length !==0){
-            return res.status(404).send("Email already exist.");
-        } 
-        await pool.query(
-            "INSERT INTO subscriptions (email) VALUES ($1);",[user_email]
-        )   
-        res.status(201).send("Email Subscription Successfully!")    
-        // console.log("The user subs email is : " , user_email);
-
-    } catch (error) {
-        console.log(error);
-        res.status(500).send("Internal Server Error");
-
-    }
+    // Send email
+    await transporter.sendMail(mailOptions);
+  } catch (error) {
+    console.error("Error sending confirmation email:", error);
+  }
 }
+
+exports.subscribe = async (req, res) => {
+  try {
+    const { user_email } = req.body;
+    if (!user_email || user_email === "") {
+      return res.status(401).send("Email address canot be an empty string");
+    }
+
+    const emailExist = await pool.query(
+      "SELECT * FROM subscriptions WHERE email = $1",
+      [user_email]
+    );
+    if (emailExist.rows.length !== 0) {
+      return res.status(404).send("Email already exist.");
+    }
+    await pool.query("INSERT INTO subscriptions (email) VALUES ($1);", [
+      user_email,
+    ]);
+    //send email
+    sendConfirmationEmail(user_email);
+
+    res.status(201).send("Email Subscription Successfully!");
+    // console.log("The user subs email is : " , user_email);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Internal Server Error");
+  }
+};
