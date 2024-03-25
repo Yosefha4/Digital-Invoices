e75a1a3b3b7cc2af91eac5379169c48a76ff347f
yosefha4 <yosefha4@ac.sce.ac.il>
2024-03-25 12:53:58 +0200

    Update input style

--- frontend/src/components/InputItem.jsx
+++ frontend/src/components/InputItem.jsx
@@ -22,8 +22,8 @@ const InputItem = ({label,inputType,placeHolder,name,changeFunc,errorFlag,errorM
     <div className={`input-item ` + errorState}>
         <label>{label}</label>
         <input type={inputType} placeholder={placeHolder} name={name} minLength={2}  onChange={handleChange} required/>
-        <i className="fas fa-check-circle"></i>
-        <i className="fas fa-exclamation-circle"></i>
+        {/* <i className="fas fa-check-circle"></i>
+        <i className="fas fa-exclamation-circle"></i> */}
         <span>{errorMessage}</span>
     </div>
   )
